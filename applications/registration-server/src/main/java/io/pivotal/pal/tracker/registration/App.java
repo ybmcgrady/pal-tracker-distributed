0987c581a648406705457b18669ba14fe756d912
bo yu <Mac@bos-MacBook-Pro.local>
2018-02-27 12:18:09 -0700

    update registration app

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -3,16 +3,19 @@ package io.pivotal.pal.tracker.registration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 import java.util.TimeZone;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({
-    "io.pivotal.pal.tracker.accounts",
-    "io.pivotal.pal.tracker.restsupport",
-    "io.pivotal.pal.tracker.projects",
-    "io.pivotal.pal.tracker.users"
+        "io.pivotal.pal.tracker.accounts",
+        "io.pivotal.pal.tracker.restsupport",
+        "io.pivotal.pal.tracker.projects",
+        "io.pivotal.pal.tracker.users",
+        "io.pivotal.pal.tracker.registration"
 })
 public class App {
     public static void main(String[] args) {

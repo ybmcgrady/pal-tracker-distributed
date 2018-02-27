6da6faca01e3cea0e298e1ec9e81c0e30b0f6956
bo yu <Mac@bos-MacBook-Pro.local>
2018-02-26 19:06:06 -0700

    change registration url

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -53,7 +53,7 @@ public class ApplicationServer {
 
     private static void waitUntilServerIsUp(String port) throws InterruptedException {
         HttpClient httpClient = new HttpClient();
-        int timeout = 720;
+        int timeout = 120;
         Instant start = Instant.now();
         boolean isUp = false;
 

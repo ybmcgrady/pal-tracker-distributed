260c01e84d8381e20a73f0263104e1edf383b86d
bo yu <bo.yu@cognizant.com>
2018-03-01 15:10:54 -0700

    zipkin solution

---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}

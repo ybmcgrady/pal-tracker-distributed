0d45e0e460384f0839b75985f4b714197602ad74
bo yu <bo.yu@cognizant.com>
2018-03-01 16:36:31 -0700

    config server solution

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -42,7 +42,8 @@ public class ApplicationServer {
             .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
             .put("APPLICATION_OAUTH_ENABLED", "false")
                 .put("SPRING_ZIPKIN_ENABLED", "false")
-            .build()
+                .put("REGISTRATION_SERVER_ENDPOINT", "http://registration-server")
+                .build()
         );
     }
 
